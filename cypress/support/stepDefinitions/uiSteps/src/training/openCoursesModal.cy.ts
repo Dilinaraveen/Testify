964039d09e75d2a0b2f9351e57950f823c2eab0e
Nethminikavindya <muthuhewaamnk.20@uom.lk>
2024-12-31 13:28:19 +0530

    search courses test

--- cypress/support/stepDefinitions/uiSteps/src/training/openCoursesModal.cy.ts
+++ cypress/support/stepDefinitions/uiSteps/src/training/openCoursesModal.cy.ts
@@ -1,6 +1,7 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { TrainingPage } from '../../page-objects/Training-page';
 
+
 const trainingPage = new TrainingPage();
 
 Given('the user is on the Training page', () => {
@@ -8,13 +9,13 @@ Given('the user is on the Training page', () => {
 });
 
 When('the user clicks the "Add New" button', () => {
-    cy.get('.ant-space-item > .ant-btn').click(); // Adjust selector if needed
+    cy.get('.ant-space-item > .ant-btn').click();
 });
 
 Then('the "Courses" modal should be visible', () => {
-    cy.get('.ant-modal-content').should('be.visible'); // Check modal visibility
+    cy.get('.ant-modal-content').should('be.visible');
 });
 
 Then('the modal header should be "Courses"', () => {
-    cy.get('.ant-modal-title').should('have.text', 'Courses'); // Verify modal title
+    cy.get('.ant-modal-title').should('have.text', 'Courses');
 });
