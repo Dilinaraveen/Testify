299c5e3db736f5b07061497c1f695d8553e643bf
RansiluSooriyahetti <ransilusooriyahetti@gmail.com>
2025-01-02 00:05:38 +0530

    update

--- cypress/support/stepDefinitions/apiSteps/post-request/post.cy.ts
+++ cypress/support/stepDefinitions/apiSteps/post-request/post.cy.ts
@@ -1,12 +1,13 @@
 import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps';
 
 let response: Cypress.Response<Book>;
+let alreadyExistResponse: Cypress.Response<{ message: string }>;
 
 Given('the API endpoint is ready', () => {
   cy.log('API endpoint is ready');
 });
 
-When('the user send a POST request to create a book', () => {
+When('the user sends a POST request to create a book', () => {
   cy.request({
     method: 'POST',
     url: `${Cypress.env('API_URL')}/api/books`, // API endpoint
@@ -31,3 +32,25 @@ Then('the response should contain the created book', () => {
     author: 'new author',
   });
 });
+
+When('the user tries to create the same book again', () => {
+    cy.request({
+      method: 'POST',
+      url: `${Cypress.env('API_URL')}/api/books`,
+      headers: {
+        Authorization: Cypress.env('API_AUTHORIZATION'),
+      },
+      body: {
+        id: 1,
+        title: 'new title',
+        author: 'new author',
+      },
+    }).then((res) => {
+        alreadyExistResponse = res;
+    });
+  });
+  
+  Then('the response should indicate the book already exists', () => {
+    expect(alreadyExistResponse.status).to.eq(208); // check for ALREADY_REPORTED status code
+    expect(alreadyExistResponse.body).to.eq('Book Already Exists');
+  });
