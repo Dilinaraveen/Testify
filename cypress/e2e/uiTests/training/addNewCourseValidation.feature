b843b77d39df83065ea393953a93931182653214
Dilinaraveen <dilinaraveen@gmail.com>
2025-01-02 16:19:45 +0530

    bug fixed

--- cypress/e2e/uiTests/training/addNewCourseValidation.feature
+++ cypress/e2e/uiTests/training/addNewCourseValidation.feature
@@ -1,23 +1,8 @@
-# This feature ensures the system validates required fields when adding a new course.
-
 Feature: Add New Course Validation
 
-  # Scenario: Validation for missing required fields while adding a course
-  # This scenario tests if the system displays an appropriate error message
-  # when a user attempts to save a course without providing the necessary information.
-
   Scenario: User attempts to add a new course without filling in required fields
-    # Given: The user navigates to the Training page
     Given the user is on the Training page
-
-    # When: The user clicks on the "Add New" button to open the modal for adding a course
     When the user clicks on the "Add New" button
-
-    # And: The "Courses" modal should appear
     And the "Courses" modal opens
-
-    # And: The user attempts to save without entering any course details
     And the user clicks on the "Save" button without entering any information
-
-    # Then: An error message should notify the user that required fields are missing
     Then the error message "This field is required" should be displayed
--- cypress/support/stepDefinitions/uiSteps/src/training/addNewCourseValidation.cy.ts
+++ cypress/support/stepDefinitions/uiSteps/src/training/addNewCourseValidation.cy.ts
@@ -1,25 +1,25 @@
-// import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-// import { TrainingPage } from '../../page-objects/Training-page';
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { TrainingPage } from '../../page-objects/Training-page';
 
-// const trainingPage = new TrainingPage();
+const trainingPage = new TrainingPage();
 
-// Given('the user is on the Training page', () => {
-//     trainingPage.visitTrainingPage();
-// });
+Given('the user is on the Training page', () => {
+    trainingPage.visitTrainingPage();
+});
 
-// When('the user clicks on the "Add New" button', () => {
-//     cy.get('.ant-space-item > .ant-btn').click();
-// });
+When('the user clicks on the "Add New" button', () => {
+    cy.get('.ant-space-item > .ant-btn').click();
+});
 
 
-// Then('the "Courses" modal opens', () => {
-//     cy.get('.ant-modal-content').should('be.visible');
-// });
+Then('the "Courses" modal opens', () => {
+    cy.get('.ant-modal-content').should('be.visible');
+});
 
-// When('the user clicks on the "Save" button without entering any information', () => {
-//     cy.get('.ant-space > :nth-child(2) > .ant-btn').click();
-// });
+When('the user clicks on the "Save" button without entering any information', () => {
+    cy.get('.ant-space > :nth-child(2) > .ant-btn').click();
+});
 
-// Then('the error message "This field is required" should be displayed', () => {
-//     cy.get('#code_help > .ant-form-item-explain-error').should('be.visible');
-// });
+Then('the error message "This field is required" should be displayed', () => {
+    cy.get('#code_help > .ant-form-item-explain-error').should('be.visible');
+});
