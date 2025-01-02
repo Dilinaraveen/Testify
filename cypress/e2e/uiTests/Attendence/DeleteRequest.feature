76554a07774b3f3210dd5becdd2dd77633f7b5aa
MWijesekara <menurawijesekara17@gmail.com>
2025-01-02 16:12:54 +0530

    Delete API Done Commit
    Delete API implemented

--- cypress/e2e/uiTests/Attendence/DeleteRequest.feature
+++
@@ -1,5 +0,0 @@
-Feature: Functionality Delete User When Logged in as User
-  Scenario: Send a Delete Request to Delete an Item as User
-    Given User log in as the user
-    When User send a Delete request to Delete
-    Then the Response should be 403 Forbidden
---
+++ cypress/support/stepDefinitions/apiSteps/Delete-Request/DeleteRequestAsUser.cy.ts
@@ -0,0 +1,30 @@
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps';
+let response;
+let idtoDelete = 5;
+
+Given('the User is logged in and the API endpoint is ready', () => {
+  cy.log('API endpoint is ready');
+});
+
+When('the user sends a Delete request to Delete a book', () => {
+  cy.request({
+    method: 'DELETE',
+    url: `${Cypress.env('API_URL')}/api/books/${idtoDelete}`, // API endpoint
+    headers: {
+      Authorization: Cypress.env('API_AUTHORIZATION_USER'), // Basic auth header
+    },
+    failOnStatusCode: false,
+  }).then((res) => {
+    response = res;
+    console.log("hello")
+    console.log(response)
+  });
+});
+
+Then('the response should be "User is not permitted"', () => {
+  expect(response.status).to.eq(403); // Check for CREATED status code
+  expect(response.body).to.have.property('message','User is not permitted.')
+});
+
+
+  
