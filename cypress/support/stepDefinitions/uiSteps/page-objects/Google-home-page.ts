c9997666842450e91df57c9ae543213c5f47a826
Dilinaraveen <dilinaraveen@gmail.com>
2025-01-02 10:52:40 +0530

    change object files

--- cypress/support/stepDefinitions/uiSteps/page-objects/Google-home-page.ts
+++
@@ -1,9 +0,0 @@
-export class GoogleHomePage {
-    SEARCH_BUTTON = '.FPdoLc > center > .gNO89b';
-
-    verifyGoogleLogo() {
-        cy.visit('https://www.google.com/')
-        cy.get(this.SEARCH_BUTTON).should('contain', 'Google Search');
-    }
-}
-
