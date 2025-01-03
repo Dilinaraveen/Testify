d5933b30452f048f5a682c7f2859c97e0581b7d2
Nethminikavindya <muthuhewaamnk.20@uom.lk>
2025-01-03 13:35:12 +0530

    get all book list test

--- cypress/support/stepDefinitions/apiSteps/Get-Request/GetAllBookList.cy.ts
+++ cypress/support/stepDefinitions/apiSteps/Get-Request/GetAllBookList.cy.ts
@@ -3,18 +3,31 @@ import { PutRequestBookApi } from '../page-objects/PutRequest';
 
 let getBooksResponse: Cypress.Response<any[]>;
 
-const putRequestBookApi = new PutRequestBookApi();
 
 Given('the API endpoint for get all book list is ready', () => {
   cy.log('API endpoint is ready for retrieving all books');
 });
 
-When('the user sends a GET request to retrieve all books', () => {
-    putRequestBookApi.ensureBookListExist([
-    { id: 1, title: 'Book One', author: 'Author One' },
-    { id: 2, title: 'Book Two', author: 'Author Two' },
-  ]);
+When('the user sends a GET request to retrieve all books when no records exist', () => {
+    
+    cy.request({
+      method: 'GET',
+      url: `${Cypress.env('API_URL')}/api/books`,
+      headers: {
+        Authorization: Cypress.env('API_AUTHORIZATION_USER'),
+      },
+    }).then((res) => {
+      getBooksResponse = res;
+    });
+  });
+  
+Then('the response should indicate an empty list of books', () => {
+    expect(getBooksResponse.status).to.eq(200);
+    expect(getBooksResponse.body).to.be.an('array').that.is.empty;
+});
 
+When('the user sends a GET request to retrieve all books', () => {
+    
   cy.request({
     method: 'GET',
     url: `${Cypress.env('API_URL')}/api/books`,
@@ -30,19 +43,4 @@ Then('the res status should be 200', () => {
   expect(getBooksResponse.status).to.eq(200);
 });
 
-Then('the response should contain a list of books', () => {
-  expect(getBooksResponse.body).to.be.an('array').that.is.not.empty;
-  expect(getBooksResponse.body).to.have.length(2);
 
-  expect(getBooksResponse.body[0]).to.deep.equal({
-    id: 1,
-    title: 'Book One',
-    author: 'Author One',
-  });
-
-  expect(getBooksResponse.body[1]).to.deep.equal({
-    id: 2,
-    title: 'Book Two',
-    author: 'Author Two',
-  });
-});
